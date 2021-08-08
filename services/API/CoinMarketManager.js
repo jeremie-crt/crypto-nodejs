a12eeff44a699efc08b1a7b3241e07463243cb77
jeremeie-crt <certain.j@live.fr>
2021-08-08 19:51:29 +0200

    commit mongo part

--- services/API/CoinMarketManager.js
+++ services/API/CoinMarketManager.js
@@ -5,6 +5,7 @@ class coinMarketManager {
     static getAllCurrenciesList(next) {
         console.log('99999');
         //coinMarketAPI.getCurrenciesListedIds('1','1027','1839','2539')
+
         coinMarketAPI.getCurrenciesListedIds();
 
     }
